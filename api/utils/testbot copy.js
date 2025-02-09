5676f74ad9a57489de9101e110611f7411a92c15
Bilal Imran <bilalimranuw@gmail.com>
2025-02-09 14:19:56 +0400

    Update testbot copy.js

--- api/utils/testbot copy.js
+++ api/utils/testbot copy.js
@@ -8,7 +8,7 @@ require('dotenv').config();
 const { NlpManager } = require('node-nlp');
 const { HfInference } = require('@huggingface/inference');
 const manager = new NlpManager({ languages: ['en'] });
-const hf = new HfInference('token');
+const hf = new HfInference('');
 
 
 
@@ -119,7 +119,7 @@ Rewritten response:
 
     const url = 'https://api-inference.huggingface.co/models/google/flan-t5-large';
     const headers = {
-        Authorization: `Bearer token`,
+        Authorization: `Bearer`,
         'Content-Type': 'application/json',
     };
 
